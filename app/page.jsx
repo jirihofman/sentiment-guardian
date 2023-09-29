a78f45d2870d1e920e1dc4b96deded8b0d920cb9
Jiří Hofman <jiri.hofman@gmail.com>
2023-09-29 19:19:23 +0200

    feat: cache page instead of component

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@ import Hero from '../components/landing/hero';
 import Feature, { ArticleTableSkeleton } from '../components/landing/feature';
 import { Suspense } from 'react';
 
+export const revalidate = 60;
+
 export default function Home() {
 
     return (
