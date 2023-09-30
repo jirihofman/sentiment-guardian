aba84dde9ff0c35d2d5389c966d4f6dbb60657c7
Jiří Hofman <jiri.hofman@gmail.com>
2023-09-30 21:44:59 +0200

    feat: remove UserButton for now - sync is done automagically - no need for purge right now

--- components/layout/header.js
+++ components/layout/header.js
@@ -3,20 +3,20 @@
 import pjson from '../../package.json';
 import { Container, Modal, Nav, Navbar } from 'react-bootstrap';
 import Link from 'next/link';
-import Buttons from '../buttons';
-import { UserButton, useSession, useUser } from '@clerk/nextjs';
+// import Buttons from '../buttons';
+// import { UserButton, useSession, useUser } from '@clerk/nextjs';
 
 export default function Header() {
 
-    const { isLoaded, isSignedIn } = useSession();
-    const { user } = useUser();
-    const isAdmin = user?.emailAddresses?.find(email => email.emailAddress === pjson.author.email);
+    // const { isLoaded, isSignedIn } = useSession();
+    // const { user } = useUser();
+    // const isAdmin = user?.emailAddresses?.find(email => email.emailAddress === pjson.author.email);
 
-    const handleSignInClick = (evt) => {
-        evt.preventDefault();
-        // Redirect to sign-in page.
-        window.location.href = '/sign-in';
-    };
+    // const handleSignInClick = (evt) => {
+    //     evt.preventDefault();
+    //     // Redirect to sign-in page.
+    //     window.location.href = '/sign-in';
+    // };
 
     return (
         <header>
@@ -34,14 +34,14 @@ export default function Header() {
                         <Nav className='me-auto'>
                             <Link passHref href="/faq" legacyBehavior><Nav.Link>FAQ</Nav.Link></Link>
                             <Nav.Link role='button' data-bs-toggle='modal' data-bs-target='#exampleModal'>About</Nav.Link>
-                            <Nav.Item>{isAdmin && <Buttons />}</Nav.Item>
-                            <Nav>
+                            {/* <Nav.Item>{isAdmin && <Buttons />}</Nav.Item> */}
+                            {/* <Nav>
                                 {!isLoaded && <div className="spinner-border spinner-border-sm text-primary" role="status">
                                     <span className="sr-only" />
                                 </div>}
                                 {!isSignedIn && isLoaded && <button className='btn-sm btn btn-outline-primary' onClick={handleSignInClick}>Sign in</button>}
                                 <UserButton afterSignOutUrl="/" />
-                            </Nav>
+                            </Nav> */}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -57,10 +57,6 @@ export default function Header() {
                             <p>{pjson.description}</p>
                             <table>
                                 <tbody>
-                                    <tr>
-                                        <th>Author</th>
-                                        <td>{pjson.author.name}</td>
-                                    </tr>
                                     <tr>
                                         <th>Version</th>
                                         <td>{pjson.version}</td>
