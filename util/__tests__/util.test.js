76462bcff5fd95e503a4c72abce0483f243bc90d
Jiří Hofman <jiri.hofman@gmail.com>
2023-09-30 19:11:23 +0200

    Update util.test.js

--- util/__tests__/util.test.js
+++ util/__tests__/util.test.js
@@ -1,17 +1,5 @@
 import { getLangIcon, niceBytes } from '../util';
 
-describe('getLangIcon', () => {
-    it('cz', () => {
-        expect(getLangIcon('cs')).toBe('🇨🇿');
-    });
-    it('en', () => {
-        expect(getLangIcon('en')).toBe('🇬🇧');
-        expect(getLangIcon()).toBe('🇬🇧');
-        expect(getLangIcon('')).toBe('🇬🇧');
-        expect(getLangIcon('foo')).toBe('🇬🇧');
-    });
-});
-
 describe.skip('niceBytes', () => {
     it('KB', () => {
         expect(niceBytes(2148)).toBe('2.1 KB');
