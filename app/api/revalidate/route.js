af73aeab28a911ffeae717358198cb6a662f2ea8
Jiří Hofman <jiri.hofman@gmail.com>
2023-10-01 23:32:19 +0200

    fix: use Response instead of NextResponse

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,11 +1,17 @@
-import { NextResponse } from 'next/server';
+/* eslint-disable no-console */
+// import { NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 
 export async function POST() {
 
+    console.log('Revalidating Tags');
     revalidateTag('article:guardian');
+    console.log('Revalidated article:guardian');
     revalidateTag('article:guardian:hits');
+    console.log('Revalidated article:guardian:hits');
     revalidateTag('category:guardian');
+    console.log('Revalidated category:guardian');
 
-    return NextResponse.json({ now: Date.now(), revalidated: true });
+    // return NextResponse.json({ now: Date.now(), revalidated: true });
+    return new Response(JSON.stringify({ now: Date.now(), revalidated: true }));
 }
