38dc986a6bd33901ab828610840e7bcebdd1e5aa
Jiří Hofman <jiri.hofman@gmail.com>
2023-10-22 22:57:36 +0200

    feat: improve SEO, add favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,21 @@ export default async function LocaleLayout({ children }) {
     );
 }
 
+/** @type {import('next').Metadata} */
 export const metadata = {
     description: pjson.description,
+    // Thanks https://css-tricks.com/emoji-as-a-favicon/
+    icons: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📰</text></svg>',
+    robots: {
+        follow: true,
+        googleBot: {
+            follow: true,
+            index: true,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+            'max-video-preview': -1,
+        },
+        index: true,
+    },
     title: pjson.displayName,
 };
