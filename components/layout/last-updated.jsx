7d0ed6eb6e4114c09f3de68a683b9916d908d75b
Jiří Hofman <jiri.hofman@gmail.com>
2023-11-29 13:00:56 +0100

    fix: add cache revalidation to last-updated

--- components/layout/last-updated.jsx
+++ components/layout/last-updated.jsx
@@ -1,8 +1,18 @@
+/** 20 minutes. Usually revalidated by CI curl call. */
+const revalidate = 60 * 20;
+
 export default async function LastUpdated() {
     const res = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/articles-last-updated`, {
         headers: {
             Authorization: 'Bearer ' + process.env.UPSTASH_REDIS_REST_TOKEN,
-        }
+        },
+        next: {
+            revalidate,
+            tags: [
+                'category:guardian',
+                'article:guardian:hits',
+            ],
+        },
     });
     const data = await res.json();
     // Format `data.result` to YYYY-MM-DD HH:MM:SS UTC
