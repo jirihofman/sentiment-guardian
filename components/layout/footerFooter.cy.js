6c47671172e07b581032470ca232ba3325bc8ac6
Jiří Hofman <jiri.hofman@gmail.com>
2023-11-29 12:09:57 +0100

    test: fix react server component test

--- components/layout/footerFooter.cy.js
+++ components/layout/footerFooter.cy.js
@@ -1,11 +1,17 @@
-import React from 'react';
+/* eslint-disable cypress/no-async-tests */
 import Footer from './footer';
 
 describe('<Footer />', () => {
-    it('renders', () => {
+    it('renders', async () => {
+
+        // Note that this is a React Server Component, so we need to mock the fetch
+        // Thanks https://www.youtube.com/watch?v=b9LH2gYubSo
+        cy.stub(window, 'fetch').resolves({
+            json: cy.stub().resolves(new Date().toISOString()),
+        });
+        // await the server component
+        const comp = await Footer();
         // see: https://on.cypress.io/mounting-react
-        cy.mount(
-            <Footer />
-        );
+        cy.mount(comp);
     });
 });
